fd567be319f041205d291f9619bed8efdf97acbb
ItsMyDay797 <m.rus1298@gmail.com>
2022-11-06 19:14:11 +0300

    Initial Commit

---
+++ PokemonSU/PokemonSUApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonSUApp.swift
+//  PokemonSU
+//
+//  Created by Марк Русаков on 6.11.22.
+//
+
+import SwiftUI
+
+@main
+struct PokemonSUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
